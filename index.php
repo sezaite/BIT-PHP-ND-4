7e2d237e6c2e0203782a5ce43089b59d6c561c5f
sezaite <martyna.sezaite@gmail.com>
2021-02-15 22:06:49 +0000

    10

--- index.php
+++ index.php
@@ -62,6 +62,9 @@ echo '<br><br><br><br>';
 print_r($nePorinis);
 echo '<br><br><br><br>';
 
+
+//KUR G UZDUOTIS??????????????????????????????????????????????????????????????????????????????????????????????????????
+
 // Suraskite pirmą (mažiausią) indeksą, kurio elemento reikšmė didesnė už 10;
 
 function didesnisUzDesimt($skaicius){
@@ -95,11 +98,135 @@ for ($i = 0; $i < $masyvoIlgis; $i = $i + 2){
 
 print_r($senasPliusNaujasMasyvas);
 
+echo '<br><br><br>------------TRECIA UZDUOTIS-----------<br><br><br><br>';
+
+// Sugeneruokite masyvą, kurio reikšmės atsitiktinės raidės A, B, C ir D, o ilgis 200. Suskaičiuokite kiek yra kiekvienos raidės.
+
+$array = [];
+$countA = 0;
+$countB = 0;
+$countC = 0;
+$countD = 0;
+
+for($i = 0; $i <200; $i++){
+    array_push($array, chr(rand(65, 68)));
+}
+
+print_r($array);
+
+for($i = 0; $i < count($array); $i++){
+    switch ($array[$i]) {
+        case 'A':
+            $countA++;
+            break;
+        case 'B':
+            $countB++;
+            break;
+        case 'C':
+            $countC++;
+            break;
+        case 'D':
+            $countD++;
+            break;
+        default:
+            echo 'kazkokia tai nesamone cia tau susigeneravo';    
+    }
+}
+echo '<br><br><br>------------KETVIRTA UZDUOTIS-----------<br><br><br><br>';
+// Išrūšiuokite 3 uždavinio masyvą pagal abecėlę.
+ 
+sort($array, SORT_STRING);
+print_r($array);
+
+echo '<br><br><br>------------PENKTA UZDUOTIS-----------<br><br><br><br>';
+// Sugeneruokite 3 masyvus pagal 3 uždavinio sąlygą. Sudėkite masyvus, sudėdami atitinkamas reikšmes. Paskaičiuokite kiek unikalių reikšmių kombinacijų gavote.
+
+$arrayA = [];
+$arrayB = [];
+$arrayC = [];
+$arrayABC = [];
+
+for($i = 0; $i <200; $i++){
+    array_push($arrayA, chr(rand(65, 68)));
+}
+
+for($i = 0; $i <200; $i++){
+    array_push($arrayB, chr(rand(65, 68)));
+}
+
+for($i = 0; $i <200; $i++){
+    array_push($arrayC, chr(rand(65, 68)));
+}
+
+for ($i = 0; $i < 200; $i++){
+    array_push($arrayABC, $arrayA[$i] . $arrayB[$i] . $arrayC[$i]);
+}
+echo '<pre>';
+print_r(array_count_values($arrayABC));
+echo '<br><br> skirtingu kombinaciju yra: ' . count((array_count_values($arrayABC)));
 
 
+echo '<br><br><br>------------SESTA UZDUOTIS-----------<br><br><br><br>';
 
+// Sugeneruokite du masyvus, kurių reikšmės yra atsitiktiniai skaičiai nuo 100 iki 999. Masyvų ilgiai 100. Masyvų reikšmės turi būti unikalios savo masyve (t.y. neturi kartotis).
 
+$masyvas = [];
+while(count($masyvas)<100){
+    $skaicius = rand(100, 999);
+    if (!in_array($skaicius, $masyvas)){
+        array_push($masyvas, $skaicius);
+    }
+}
 
+echo '<pre>';
+print_r($masyvas);
 
+$masyvas2 = [];
+while(count($masyvas2)<100){
+    $skaicius = rand(100, 999);
+    if (!in_array($skaicius, $masyvas2)){
+        array_push($masyvas2, $skaicius);
+    }
+}
 
-    
+echo '<pre>';
+print_r($masyvas2);
+
+echo '<br><br><br>------------SEPTINTA UZDUOTIS-----------<br><br><br><br>';
+
+// Sugeneruokite masyvą, kuris būtų sudarytas iš reikšmių, kurios yra pirmame 6 uždavinio masyve, bet nėra antrame 6 uždavinio masyve.
+
+$arr = array_diff($masyvas, $masyvas2);
+echo 'pre';
+print_r($arr);
+
+echo '<br><br><br>------------ASTUNTA UZDUOTIS-----------<br><br><br><br>';
+// Sugeneruokite masyvą iš elementų, kurie kartojasi abiejuose 6 uždavinio masyvuose.
+
+$arr = array_intersect($masyvas, $masyvas2);
+echo 'pre';
+print_r($arr);
+
+echo '<br><br><br>------------DEVINTA UZDUOTIS-----------<br><br><br><br>';
+// Sugeneruokite masyvą, kurio indeksus sudarytų pirmo 6 uždavinio masyvo reikšmės, o jo reikšmės iš būtų antrojo masyvo.
+
+echo '<pre>';
+print_r(array_slice(array_combine($masyvas2, $masyvas), 0, 6, true));
+
+echo '<br><br><br>------------DESIMTA UZDUOTIS-----------<br><br><br><br>';
+// Sugeneruokite 10 skaičių masyvą pagal taisyklę: Du pirmi skaičiai- atsitiktiniai nuo 5 iki 25. Trečias, pirmo ir antro suma. Ketvirtas- antro ir trečio suma. Penktas trečio ir ketvirto suma ir t.t.
+
+$masyvas10 = [rand(5, 25), rand(5, 25)];
+$i = 0;
+while(count($masyvas10) < 10){
+    array_push($masyvas10, ($masyvas10[$i] + $masyvas10[$i+1]));
+    $i++;
+}
+echo '<pre>';
+print_r($masyvas10);
+
+/////currrrenttt???
+
+echo '<br><br><br>------------VIENUOLIKTA UZDUOTIS-----------<br><br><br><br>';
+
+// Sugeneruokite 101 elemento masyvą su atsitiktiniais skaičiais nuo 0 iki 300. Reikšmes kurios tame masyve yra ne unikalios pergeneruokite iš naujo taip, kad visos reikšmės masyve būtų unikalios. Išrūšiuokite masyvą taip, kad jo didžiausia reikšmė būtų masyvo viduryje, o einant nuo jos link masyvo pradžios ir pabaigos reikšmės mažėtų. Paskaičiuokite pirmos ir antros masyvo dalies sumas (neskaičiuojant vidurinės). Jeigu sumų skirtumas (modulis, absoliutus dydis) yra didesnis nei | 30 | rūšiavimą kartokite. (Kad sumos nesiskirtų viena nuo kitos daugiau nei per 30)
